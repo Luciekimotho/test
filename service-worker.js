13ebbe13101ed1fe062731ff2437788b7d1f0a74
lkimotho <lkimotho@intellisoftkenya.com>
2020-09-04 12:57:16 +0300

    SDG

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sdgportalreact/precache-manifest.108a98533bacbd45eed112341b36d1b7.js"
+  "/sdgportalreact/precache-manifest.55ab82bc606e4cc06b06382b94af8174.js"
 );
 
 self.addEventListener('message', (event) => {
