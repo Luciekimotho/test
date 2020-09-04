bcc5720acf32729536f84617cde044b1dfd0e3ca
lkimotho <lkimotho@intellisoftkenya.com>
2020-09-04 10:43:24 +0300

    SDG

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.d589b0253961786ef01ddb8344e010b6.js"
+  "/sdgportalreact/precache-manifest.32a929e2f275b000f6973c2d4ad9bfa9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sdgportalreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- static/js/xlsx.62f2d646.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsdg-portal"]=this["webpackJsonpsdg-portal"]||[]).push([[2],{620:function(n,o){},705:function(n,o){},706:function(n,o){}}]);
-//# sourceMappingURL=xlsx.62f2d646.chunk.js.map
